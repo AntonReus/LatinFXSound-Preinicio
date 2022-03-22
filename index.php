1910e0ddd3557866509cea98cae5b4173c87a7df
AntonReus <mhernandezn1602@alumno.ipn.mx>
2022-03-21 23:00:12 -0600

    TraductorOk

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include('config/connection.php') ?>
+<?php //include('config/connection.php') ?>
 <?php include"language/config.php" ?>
 
 <!DOCTYPE html>
@@ -29,7 +29,7 @@
     <!-- MENU BAR -->
     <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container">
-            <div><a class="navbar-brand" href="index.html">LATINFXSOUND***</a></div>
+            <div><a class="navbar-brand" href="index.php">LATINFXSOUND***</a></div>
             <div><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -103,27 +103,22 @@
             <div class="row">
                 <div class="d-flex flex-column justify-content-left ml-lg-auto mr-lg-5 col-lg-5 col-md-6 col-12">
                     <h2 class="mb-3 text-white" data-aos="fade-up"><?php echo $lang['Ini1']?></h2>
-
                     <h6 class="mb-4 text-white" data-aos="fade-up"><?php echo $lang['Ini2']?></h6>
                     <h6 class="mb-4 text-white" data-aos="fade-up"><?php echo $lang['Ini3']?></h6>
                     <h6 class="mb-4 text-white" data-aos="fade-up"><?php echo $lang['Ini4']?></h6>
                     <h6 class="mb-4 text-white" data-aos="fade-up"><?php echo $lang['Ini5']?></h6>
-                    <a href="web_services/registrer.php" class="btn custom-btn bg-color mt-3" data-aos="fade-up" data-aos-delay="300" data-toggle="modal" data-target="web_services/registrer.php"><?php echo $lang['Nav4']?></a>
+                    <a href="web_services/registrer.php" class="btn custom-btn bg-color mt-3" data-aos="fade-up" data-aos-delay="300" data-toggle="modal" data-target="web_services/registrer.php"><?php echo $lang['Ini11']?></a>
                 </div>
                 <div class="mr-lg-auto mt-3 col-lg-4 col-md-6 col-12">
-                     <div class="about-working-hours">
-                          <div>
-                                <h2 class="mb-4 text-white" data-aos="fade-up" data-aos-delay="500"><?php echo $lang['Ini6']?></h2>
-
-                               <strong class="d-block" data-aos="fade-up" data-aos-delay="600"><?php echo $lang['Ini7']?></strong>
-
-                               <strong class="mt-3 d-block" data-aos="fade-up" data-aos-delay="700"><?php echo $lang['Ini8']?></strong>
-
-                               <strong class="mt-3 d-block" data-aos="fade-up" data-aos-delay="700"><?php echo $lang['Ini9']?></strong>
-
-                                <strong class="mt-3 d-block" data-aos="fade-up" data-aos-delay="700"><?php echo $lang['Ini10']?></strong>
-                          </div>
-                     </div>
+                    <div class="about-working-hours">
+                        <div>
+                            <h2 class="mb-4 text-white" data-aos="fade-up" data-aos-delay="500"><?php echo $lang['Ini6']?></h2>
+                            <strong class="d-block" data-aos="fade-up" data-aos-delay="600"><?php echo $lang['Ini7']?></strong>
+                            <strong class="mt-3 d-block" data-aos="fade-up" data-aos-delay="700"><?php echo $lang['Ini8']?></strong>
+                            <strong class="mt-3 d-block" data-aos="fade-up" data-aos-delay="700"><?php echo $lang['Ini9']?></strong>
+                            <strong class="mt-3 d-block" data-aos="fade-up" data-aos-delay="700"><?php echo $lang['Ini10']?></strong> 
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -135,20 +130,14 @@
                     <div class="row">
                             <div class="mt-lg-5 mb-lg-0 mb-4 col-lg-5 col-md-10 mx-auto col-12">
                                 <h2 class="mb-4" data-aos="fade-up" data-aos-delay="300"><?php echo $lang['About1']?></h2>
-
                                 <p data-aos="fade-up" data-aos-delay="400"><?php echo $lang['About2']?></p>
-
                             </div>
-
                             <div class="ml-lg-auto col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="700">
                                 <div class="team-thumb">
                                     <img src="images/pmusical.jpg" class="img-fluid" alt="Trainer">
-
                                     <div class="team-info d-flex flex-column">
-
                                         <h3><?php echo $lang['About3']?></h3>
-                                        <span>sonidos originales</span>
-
+                                        <span><?php echo $lang['About4']?></span>
                                         <ul class="social-icon mt-3">
                                             <li><a href="#" class="fa fa-twitter"></a></li>
                                             <li><a href="#" class="fa fa-instagram"></a></li>
@@ -156,21 +145,15 @@
                                     </div>
                                 </div>
                             </div>
-
                             <div class="mr-lg-auto mt-5 mt-lg-0 mt-md-0 col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="800">
                                 <div class="team-thumb">
                                     <img src="images/persona.jpeg" class="img-fluid" alt="Trainer">
-
                                     <div class="team-info d-flex flex-column">
-
-                                        <h3>Calidad</h3>
-                                        <span>Contenido de calidad</span>
-
-                                        
+                                        <h3><?php echo $lang['About5']?></h3>
+                                        <span><?php echo $lang['About6']?></span>
                                     </div>
                                 </div>
                             </div>
-
                     </div>
                </div>
      </section>
@@ -181,9 +164,9 @@
                     <div class="row">
 
                             <div class="col-lg-12 col-12 text-center mb-5">
-                                <h6 data-aos="fade-up">Los mejores sonidos</h6>
+                                <h6 data-aos="fade-up"><?php echo $lang['Sound1']?></h6>
 
-                                <h2 data-aos="fade-up" data-aos-delay="200">Lo mas descargado</h2>
+                                <h2 data-aos="fade-up" data-aos-delay="200"><?php echo $lang['Sound2']?></h2>
                              </div>
 
                             <div class="col-lg-4 col-md-6 col-12" data-aos="fade-up" data-aos-delay="400">
@@ -191,13 +174,13 @@
                                     <img src="images/class/cocinando.jpeg" class="img-fluid" alt="Class">
 
                                     <div class="class-info">
-                                        <h3 class="mb-1">Sonido de Cocina</h3>
+                                        <h3 class="mb-1"><?php echo $lang['Sound3']?></h3>
 
-                                        <span><strong>By</strong> - LatinFXsound</span>
+                                        <span><strong><?php echo $lang['Sound4']?></span>
 
-                                        <span class="class-price">$50</span>
+                                        <span class="class-price"><?php echo $lang['Sound5']?></span>
 
-                                        <p class="mt-3">Cascaron de huevo rompiendoce, salteado de aceite, sonido de rebanar</p>
+                                        <p class="mt-3"><?php echo $lang['Sound6']?></p>
                                     </div>
                                 </div>
                             </div>
@@ -207,13 +190,13 @@
                                     <img src="images/class/transportes.jpeg" class="img-fluid" alt="Class">
 
                                     <div class="class-info">
-                                        <h3 class="mb-1">Sonidos de transportes</h3>
+                                        <h3 class="mb-1"><?php echo $lang['Sound7']?></h3>
 
-                                        <span><strong>By</strong> - LatinFXsound</span>
+                                        <span><strong><?php echo $lang['Sound8']?></span>
 
-                                        <span class="class-price">$66</span>
+                                        <span class="class-price"><?php echo $lang['Sound9']?></span>
 
-                                        <p class="mt-3">Claxon, encendido de auto, sonido de acelerar</p>
+                                        <p class="mt-3"><?php echo $lang['Sound10']?></p>
                                     </div>
                                 </div>
                             </div>
@@ -223,13 +206,13 @@
                                     <img src="images/class/naturaleza.jpeg" class="img-fluid" alt="Class">
 
                                     <div class="class-info">
-                                        <h3 class="mb-1">Sonidos naturales</h3>
+                                        <h3 class="mb-1"><?php echo $lang['Sound11']?></h3>
 
-                                        <span><strong>By</strong> - LatinFXsound</span>
+                                        <span><strong><?php echo $lang['Sound12']?></span>
 
-                                        <span class="class-price">$75</span>
+                                        <span class="class-price"><?php echo $lang['Sound13']?></span>
 
-                                        <p class="mt-3">Canto de pajaros, sonido del viento, sonido de olas</p>
+                                        <p class="mt-3"><?php echo $lang['Sound14']?></p>
                                     </div>
                                 </div>
                             </div>
@@ -244,23 +227,23 @@
                <div class="row">
 
                     <div class="ml-auto col-lg-5 col-md-6 col-12">
-                        <h2 class="mb-4 pb-2" data-aos="fade-up" data-aos-delay="200">Alguna duda? envianos un correo</h2>
+                        <h2 class="mb-4 pb-2" data-aos="fade-up" data-aos-delay="200"><?php echo $lang['Cont1']?></h2>
 
                         <form action="#" method="post" class="contact-form webform" data-aos="fade-up" data-aos-delay="400" role="form">
-                            <input type="text" class="form-control" name="cf-name" placeholder="Nombre">
+                            <input type="text" class="form-control" name="cf-name" placeholder="<?php echo $lang['Cont2']?>">
 
-                            <input type="email" class="form-control" name="cf-email" placeholder="Email">
+                            <input type="email" class="form-control" name="cf-email" placeholder="<?php echo $lang['Cont3']?>">
 
-                            <textarea class="form-control" rows="5" name="cf-message" placeholder="Mensaje"></textarea>
+                            <textarea class="form-control" rows="5" name="cf-message" placeholder="<?php echo $lang['Cont4']?>"></textarea>
 
-                            <button type="submit" class="form-control" id="submit-button" name="submit">Enviar mensaje</button>
+                            <button type="submit" class="form-control" id="submit-button" name="submit"><?php echo $lang['Cont5']?></button>
                         </form>
                     </div>
                
                     <div class="mx-auto mt-4 mt-lg-0 mt-md-0 col-lg-5 col-md-6 col-12">
-                        <h2 class="mb-4" data-aos="fade-up" data-aos-delay="600">Donde puedes<span>encontrarnos</span></h2>
+                        <h2 class="mb-4" data-aos="fade-up" data-aos-delay="600"><?php echo $lang['Cont6']?></span></h2>
 
-                        <p data-aos="fade-up" data-aos-delay="800"><i class="fa fa-map-marker mr-1"></i> Direccion del cliente</p>
+                        <p data-aos="fade-up" data-aos-delay="800"><i class="fa fa-map-marker mr-1"></i><?php echo $lang['Cont6']?></p>
 <!-- How to change your own map point
     1. Ir a google map
     2. Click en la localidad del cliente
@@ -282,9 +265,9 @@
                <div class="row">
 
                     <div class="ml-auto col-lg-4 col-md-5">
-                        <p class="copyright-text">Copyright &copy; 2020 LatinFXsound
+                        <p class="copyright-text">Copyright &copy; 2020 LatinFXSound
                         
-                        <br>Diseño: <a href="https://www.empresa.com">Empresa</a></p>
+                        <br></i><?php echo $lang['Cont7']?>: <a href="https://www.empresa.com">Empresa</a></p>
                     </div>
 
                     <div class="d-flex justify-content-center mx-auto col-lg-5 col-md-7 col-12">
@@ -305,7 +288,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h2 class="modal-title" id="membershipFormLabel">Inicio de Sesion</h2>
+                    <h2 class="modal-title" id="membershipFormLabel"><?php echo $lang['InSes1']?></h2>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -314,16 +297,16 @@
             <form method="post" class="membership-form webform" role="form" action="">
                     <?php include('web_services/errors.php'); ?>
                     <div class="input-group">
-                        <input type="email" class="form-control" name="user_email" placeholder="Direccion email">
+                        <input type="email" class="form-control" name="user_email" placeholder="<?php echo $lang['InSes2']?>">
                     </div>
                     <div class="input-group">
-                        <input type="password" class="form-control" name="user_password" placeholder="Contraseña">
+                        <input type="password" class="form-control" name="user_password" placeholder="<?php echo $lang['InSes3']?>">
                     </div>
                     <div class="input-group">
-                        <button type="submit" class="form-control" id="login_user" name="login_user">Continuar</button>
+                        <button type="submit" class="form-control" id="login_user" name="login_user"><?php echo $lang['InSes4']?></button>
                     </div>
                     <br><p>
-                        Aun no te has registrado? <a href="web_services/registrer.php">Registrate ahora mismo</a>
+                    <?php echo $lang['InSes5']?> <a href="web_services/registrer.php"><?php echo $lang['InSes6']?></a>
                     </p>
                 </form>
             </div>
--- web_services/registrer.php
+++ web_services/registrer.php
@@ -1,9 +1,11 @@
-<?php include('../config/connection.php') ?>
+<?php// include('../config/connection.php') ?>
+<?php include"../language/config.php" ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 
-     <title>LATINFXSOUND*** - Registro</title>
+     <title>LATINFXSOUND*** - <?php echo $lang['Reg1']?></title>
 
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=Edge">
@@ -21,9 +23,54 @@
 
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
+            <div><a class="navbar-brand" href="../index.php">LATINFXSOUND***</a></div>
+            <div><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button></div>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-lg-auto">
+                    <li class="nav-item">
+                        <a href="#home" class="nav-link smoothScroll" id="Inicio"><?php echo $lang['Nav1']?></a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#about" class="nav-link smoothScroll" key="Acerca"><?php echo $lang['Nav2']?></a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#sonidos" class="nav-link smoothScroll"><?php echo $lang['Nav3']?></a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#" class="nav-link smoothScroll" data-toggle="modal" data-target="#iniciarSesion"><?php echo $lang['Nav4']?></a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#contact" class="nav-link smoothScroll"><?php echo $lang['Nav5']?></a>
+                    </li>
+                </ul>
+                <ul class="social-icon ml-lg-3">
+                    <li><a href="https://fb.com/tooplate" class="fa fa-facebook"></a></li>
+                    <li><a href="#" class="fa fa-twitter"></a></li>
+                    <li><a href="#" class="fa fa-instagram"></a></li>
+                </ul>
+                <ul class="social-icon ml-lg-3">
+                    <li>
+                        <a href="registrer.php?lang=es"><?php echo $lang['es']?></a>
+                        <a href="registrer.php?lang=en"><?php echo $lang['en']?></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
     <section class="registrer bg-overlay modal-dialog-center" role="document">
         <div class="modal-content modal-dialog">
-            <div class="modal-header" ass="modal-title" id="membershipFormLabel"><h2>Crear nueva cuenta</h2>
+            <div class="modal-header" ass="modal-title" id="membershipFormLabel"><h2><?php echo $lang['Reg2']?></h2>
                 <a type="button" class="close" data-dismiss="modal" aria-label="Close" href="../index.php">
                     <span aria-hidden="true">&times;</span>
                 </a>
@@ -32,28 +79,28 @@
                 <form class="registrer_form" id="registrer_form" role="form" method="post" action="">
                     <?php include('errors.php'); ?>
                     <div class="input-group">
-                        <input type="email" class="form-control" id="user_email" name="user_email" placeholder="Direccion email">
+                        <input type="email" class="form-control" id="user_email" name="user_email" placeholder="<?php echo $lang['Reg3']?>">
                         <span id="email_error"></span>
                     </div>
                     <div class="input-group">
-                        <input type="password" class="form-control" id="user_password_1" name="user_password_1" placeholder="Contraseña">
+                        <input type="password" class="form-control" id="user_password_1" name="user_password_1" placeholder="<?php echo $lang['Reg4']?>">
                         <span id="password_error" class="ch_error"></span>
                     </div>
                     <div class="input-group">
-                        <input type="password" class="form-control" id="user_password_2" name="user_password_2" placeholder="Vuelva a escribir su contraseña">
+                        <input type="password" class="form-control" id="user_password_2" name="user_password_2" placeholder="<?php echo $lang['Reg5']?>">
                         <span id="password_error" class="ch_error"></span>
                     </div>
                     <p><div class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input" id="signup-agree_1" name="signup-agree_1">
-                        <label class="custom-control-label text-small text-muted" for="signup-agree_1">Acepto los <a href="#">Terms &amp;Conditions</a>
+                        <label class="custom-control-label text-small text-muted" for="signup-agree_1"><?php echo $lang['Reg6']?> <a href="#"><?php echo $lang['Reg7']?></a>
                         </label>
                     </div>
                     <p><div class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input" id="signup-agree_2" name="signup-agree_2">
-                        <label class="custom-control-label text-small text-muted" for="signup-agree_2">Tengo mas de 18 años</label>
+                        <label class="custom-control-label text-small text-muted" for="signup-agree_2"><?php echo $lang['Reg8']?></label>
                     </div>
                     <div class="input-group">
-                        <button type="submit" class="form-control" name="reg_user" href="index.php">Registrate</button>
+                        <button type="submit" class="form-control" name="reg_user" href="index.php"><?php echo $lang['Reg1']?></button>
                     </div>
                 </form>
             </div>
